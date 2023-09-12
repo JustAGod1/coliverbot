0d41c1ce6ebb8c1385c08772fb37d28ed9b577f3
victor <vi350@inbox.ru>
2023-09-12 21:58:00 +0300

    feat: fix

--- models/base.py
+++ models/base.py
@@ -1,7 +1,8 @@
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
 
-class Base(DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
     def __repr__(self):
